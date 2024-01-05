660a31dad0bd795f27b2c5332ce5dfc15bc1a2af
Diwanshu <saidiwanshu1880@gmail.com>
2024-01-05 23:49:09 +0530

    Optimized The Function

--- src/AutoResizeTextarea.tsx
+++ src/AutoResizeTextarea.tsx
@@ -4,8 +4,19 @@ export interface ButtonProps extends React.HTMLAttributes<HTMLTextAreaElement> {
   value?: string;
   setValue?: React.Dispatch<React.SetStateAction<string>>;
   placeholder?: string;
-  autoResize?: Boolean;
+  autoResize?: boolean;
 }
+
+const ExtractMaxHeightValue = (
+  maxHeightMap: CSSStyleValue | undefined
+): number => {
+  let maxHeightNumber: number = Infinity;
+  if (maxHeightMap && "value" in maxHeightMap) {
+    maxHeightNumber =
+      maxHeightMap.value === "none" ? Infinity : (maxHeightMap.value as number);
+  }
+  return maxHeightNumber;
+};
 
 const AutoResizeTextarea = ({
   id,
@@ -13,33 +24,24 @@ const AutoResizeTextarea = ({
   style,
   value,
   setValue,
+
   placeholder = "Type here...",
   autoResize = false,
   ...otherProps
 }: ButtonProps) => {
-  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    if (setValue) {
-      setValue(e.target.value);
-    }
-  };
+  const handleChange = setValue
+    ? (e: React.ChangeEvent<HTMLTextAreaElement>) =>
+        setValue(e.currentTarget.value)
+    : undefined;
 
   const autoResizeTextarea = (e: React.FormEvent<HTMLTextAreaElement>) => {
     if (autoResize) {
       const textarea = e.currentTarget;
       const computedStyles = textarea.computedStyleMap();
       const maxHeightMap = computedStyles.get("max-height");
-      if (maxHeightMap && "value" in maxHeightMap) {
-        const maxHeight =
-          maxHeightMap.value === "none"
-            ? Infinity
-            : (maxHeightMap.value as number);
-
-        textarea.style.height = "1px";
-        textarea.style.height = `${Math.min(
-          maxHeight,
-          textarea.scrollHeight
-        )}px`;
-      }
+      const maxHeight = ExtractMaxHeightValue(maxHeightMap);
+      textarea.style.height = "1px";
+      textarea.style.height = `${Math.min(maxHeight, textarea.scrollHeight)}px`;
     }
   };
 
